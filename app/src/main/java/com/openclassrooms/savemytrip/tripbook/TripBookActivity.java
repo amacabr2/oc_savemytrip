98facdd11193bc34ef89a7eea7b60f519be4d38f
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-09-24 10:57:18 +0200

    Appel des méthodes pour lire et écrire dans le stokage externe

--- app/src/main/java/com/openclassrooms/savemytrip/tripbook/TripBookActivity.java
+++ app/src/main/java/com/openclassrooms/savemytrip/tripbook/TripBookActivity.java
@@ -2,37 +2,56 @@ package com.openclassrooms.savemytrip.tripbook;
 
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.openclassrooms.savemytrip.R;
 import com.openclassrooms.savemytrip.base.BaseActivity;
+import com.openclassrooms.savemytrip.utils.StorageUtils;
 
 import butterknife.BindView;
 import butterknife.OnCheckedChanged;
 
+import static android.view.View.*;
+import static com.openclassrooms.savemytrip.utils.StorageUtils.*;
+
 public class TripBookActivity extends BaseActivity {
 
     //FOR DESIGN
-    @BindView(R.id.trip_book_activity_external_choice) LinearLayout linearLayoutExternalChoice;
-    @BindView(R.id.trip_book_activity_internal_choice) LinearLayout linearLayoutInternalChoice;
-    @BindView(R.id.trip_book_activity_radio_external) RadioButton radioButtonExternalChoice;
-    @BindView(R.id.trip_book_activity_radio_public) RadioButton radioButtonExternalPublicChoice;
-    @BindView(R.id.trip_book_activity_radio_volatile) RadioButton radioButtonInternalVolatileChoice;
-    @BindView(R.id.trip_book_activity_edit_text) EditText editText;
+    @BindView(R.id.trip_book_activity_external_choice)
+    LinearLayout linearLayoutExternalChoice;
+    @BindView(R.id.trip_book_activity_internal_choice)
+    LinearLayout linearLayoutInternalChoice;
+    @BindView(R.id.trip_book_activity_radio_external)
+    RadioButton radioButtonExternalChoice;
+    @BindView(R.id.trip_book_activity_radio_public)
+    RadioButton radioButtonExternalPublicChoice;
+    @BindView(R.id.trip_book_activity_radio_volatile)
+    RadioButton radioButtonInternalVolatileChoice;
+    @BindView(R.id.trip_book_activity_edit_text)
+    EditText editText;
 
-    @Override
-    public int getLayoutContentViewID() { return R.layout.activity_trip_book; }
+    private static final String FILENAME = "tripBook.txt";
+    private static final String FOLDERNAME = "bookTrip";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.configureToolbar();
+        this.readFromStorage();
+    }
+
+    @Override
+    public int getLayoutContentViewID() {
+        return R.layout.activity_trip_book;
     }
 
     @Override
@@ -49,7 +68,7 @@ public class TripBookActivity extends BaseActivity {
                 /*TODO*/
                 return true;
             case R.id.action_save:
-                /*TODO*/
+                this.save();
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -66,14 +85,57 @@ public class TripBookActivity extends BaseActivity {
         if (isChecked) {
             switch (button.getId()) {
                 case R.id.trip_book_activity_radio_internal:
-                    this.linearLayoutExternalChoice.setVisibility(View.GONE);
-                    this.linearLayoutInternalChoice.setVisibility(View.VISIBLE);
+                    this.linearLayoutExternalChoice.setVisibility(GONE);
+                    this.linearLayoutInternalChoice.setVisibility(VISIBLE);
                     break;
                 case R.id.trip_book_activity_radio_external:
-                    this.linearLayoutExternalChoice.setVisibility(View.VISIBLE);
-                    this.linearLayoutInternalChoice.setVisibility(View.GONE);
+                    this.linearLayoutExternalChoice.setVisibility(VISIBLE);
+                    this.linearLayoutInternalChoice.setVisibility(GONE);
                     break;
             }
+        }
+
+        this.readFromStorage();
+    }
+
+    private void save(){
+        if (this.radioButtonExternalChoice.isChecked()){
+            this.writeOnExternalStorage(); //Save on external storage
+        } else {
+            //TODO: Save on internal storage
+        }
+    }
+
+    // ----------------------------------
+    // UTILS - STORAGE
+    // ----------------------------------
+
+    private void readFromStorage(){
+        if (this.radioButtonExternalChoice.isChecked()){
+            if (isExternalStorageReadable()){
+                // EXTERNAL
+                if (radioButtonExternalPublicChoice.isChecked()){
+                    // External - Public
+                    this.editText.setText(getTextFromStorage(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS),this, FILENAME, FOLDERNAME));
+                } else {
+                    // External - Privatex
+                    this.editText.setText(getTextFromStorage(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), this, FILENAME, FOLDERNAME));
+                }
+            }
+        } else {
+            // TODO : READ FROM INTERNAL STORAGE
+        }
+    }
+
+    private void writeOnExternalStorage(){
+        if (isExternalStorageWritable()){
+            if (radioButtonExternalPublicChoice.isChecked()){
+                setTextInStorage(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), this, FILENAME, FOLDERNAME, this.editText.getText().toString());
+            } else {
+                setTextInStorage(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), this, FILENAME, FOLDERNAME, this.editText.getText().toString());
+            }
+        } else {
+            Toast.makeText(this, getString(R.string.external_storage_impossible_create_file), Toast.LENGTH_LONG).show();
         }
     }
 }
